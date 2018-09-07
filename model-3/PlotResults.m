1d011e7b1e15846cef51085d2b4305d2154d10ed
Hamed Rezayat <43059508+Ewindar@users.noreply.github.com>
2018-09-07 14:51:26 +0430

    Add files via upload

---
+++ model-3/PlotResults.m
@@ -0,0 +1,46 @@
+function PlotResults(t,y,name)
+
+    figure;
+    
+    % t and y
+    subplot(2,2,1);
+    plot(y,'k');
+    hold on;
+    plot(t,'r:');
+    legend('Outputs','Targets');
+    title(name);
+	xlabel('Samples');
+	ylabel('Output and Target');
+    
+    % Correlation Plot
+    subplot(2,2,2);
+    plot(t,y,'ko');
+    hold on;
+    xmin=min(min(t),min(y));
+    xmax=max(max(t),max(y));
+    plot([xmin xmax],[xmin xmax],'b','LineWidth',2);
+    R=corr(t',y');
+    title(['R = ' num2str(R)]);
+	xlabel('Target');
+	ylabel('Output');
+    
+    % e
+    subplot(2,2,3);
+    e=t-y;
+    plot(e,'b');
+    legend('Error');
+    MSE=mean(e.^2);
+    RMSE=sqrt(MSE);
+    title(['MSE = ' num2str(MSE) ', RMSE = ' num2str(RMSE)]);
+	xlabel('Samples');
+	ylabel('Error');
+    
+    subplot(2,2,4);
+    histfit(e,50);
+    eMean=mean(e);
+    eStd=std(e);
+    title(['\mu = ' num2str(eMean) ', \sigma = ' num2str(eStd)]);
+	xlabel('Error');
+	ylabel('Number of Instances');
+    
+end
